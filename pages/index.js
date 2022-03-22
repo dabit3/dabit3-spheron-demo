09ebce9d31cccda3b0f6e772ac2f5901306a8554
dabit3 <dabit3@gmail.com>
2022-03-22 15:37:06 -0400

    removed next image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -29,7 +28,7 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            Vercel
           </span>
         </a>
       </footer>
